2a41ef6a973bf637ce7ee662de460f08e69943e1
Dan Kubb <dan.kubb@gmail.com>
2011-05-27 11:07:38 -0700

    Mixin unary optimization methods into Negation optimizer

--- lib/veritas/optimizer/function/connective/negation.rb
+++ lib/veritas/optimizer/function/connective/negation.rb
@@ -7,34 +7,7 @@ module Veritas
 
         # Abstract base class representing Negation optimizations
         class Negation < Optimizer
-
-          # The optimized operand
-          #
-          # @return [Function]
-          #
-          # @api private
-          attr_reader :operand
-
-          # Initialize a Unary optimizer
-          #
-          # @return [undefined]
-          #
-          # @api private
-          def initialize(*)
-            super
-            @operand = optimize_operand
-          end
-
-        private
-
-          # Optimize the operand
-          #
-          # @return [Function]
-          #
-          # @api private
-          def optimize_operand
-            Function::Binary.optimize_operand(operation.operand)
-          end
+          include Unary
 
           # Optimize when the operand can be inverted
           class InvertibleOperand < self
