1c5b43bba0094b27e4e9fe30fdd7eed52e74aa61
Dan Kubb <dan.kubb@gmail.com>
2012-05-25 10:55:06 -0700

    Add specs for Limit and Offset operations
    * Neither operation should allow insertions

--- spec/integration/veritas/relation/operation/insertion/optimize_spec.rb
+++ spec/integration/veritas/relation/operation/insertion/optimize_spec.rb
@@ -109,4 +109,22 @@ describe Relation::Operation::Insertion, '#optimize' do
       should equal(object)
     end
   end
+
+  context 'left is a limit relation' do
+    let(:left)  { original_left.sort_by  { header }.take(1) }
+    let(:right) { original_right.sort_by { header }.take(1) }
+
+    it 'does not push-down insertions' do
+      should equal(object)
+    end
+  end
+
+  context 'left is an offset relation' do
+    let(:left)  { original_left.sort_by  { header }.drop(1) }
+    let(:right) { original_right.sort_by { header }.drop(1) }
+
+    it 'does not push-down insertions' do
+      should equal(object)
+    end
+  end
 end
