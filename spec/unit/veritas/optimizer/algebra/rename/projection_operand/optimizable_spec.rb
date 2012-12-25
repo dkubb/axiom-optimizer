8a8cf14f32fa9763ff94779d4638647a1621975d
Dan Kubb <dan.kubb@gmail.com>
2012-12-25 01:34:48 -0800

    Add spec to cover new mutation

--- spec/unit/veritas/optimizer/algebra/rename/projection_operand/optimizable_spec.rb
+++ spec/unit/veritas/optimizer/algebra/rename/projection_operand/optimizable_spec.rb
@@ -5,12 +5,19 @@ require 'spec_helper'
 describe Optimizer::Algebra::Rename::ProjectionOperand, '#optimizable?' do
   subject { object.optimizable? }
 
-  let(:header) { Relation::Header.coerce([ [ :one, Integer ], [ :two, Integer ] ]) }
-  let(:base)   { Relation.new(header, [ [ 1, 2 ] ].each)                           }
-  let(:object) { described_class.new(relation)                                     }
+  let(:header) { Relation::Header.coerce([ [ :one, Integer ], [ :two, Integer ], [ :three, Integer ] ]) }
+  let(:base)   { Relation.new(header, [ [ 1, 2 ] ].each)                                                }
+  let(:object) { described_class.new(relation)                                                          }
 
   before do
     object.operation.should be_kind_of(Algebra::Rename)
+  end
+
+  context 'when the operand is a projection and the aliases do not conflict with a removed column' do
+    let(:operand)  { base.project([ :one, :two ])                 }
+    let(:relation) { operand.rename(:one => :other, :two => :one) }
+
+    it { should be(true) }
   end
 
   context 'when the operand is a projection and the alias does not conflict with a removed column' do
