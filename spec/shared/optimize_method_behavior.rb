faa7524aa22b75ca04e24d322325b5d7bbfb5dad
Dan Kubb <dan.kubb@gmail.com>
2014-01-23 21:50:44 -0800

    Fix tests relying on deprecated interfaces

--- spec/shared/optimize_method_behavior.rb
+++ spec/shared/optimize_method_behavior.rb
@@ -2,15 +2,4 @@
 
 shared_examples_for 'an optimize method' do
   it_should_behave_like 'an idempotent method'
-
-  it 'it memoizes itself for #optimize' do
-    optimized = subject
-    expect(optimized.memoized(:optimize)).to be(optimized)
-  end
-
-  it 'does not optimize further' do
-    optimized = subject
-    optimized.memoize(:optimize, nil)  # clear memoized value
-    expect(optimized.optimize).to eql(optimized)
-  end
 end
